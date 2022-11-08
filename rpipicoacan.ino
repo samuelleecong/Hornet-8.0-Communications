04e1f98378854ef6595b512e3413bbd431bdda43
crdl2crdl <95226664+crdl2crdl@users.noreply.github.com>
2022-11-08 09:53:40 +0800

    Add files via upload

---
+++ rpipicoacan.ino
@@ -0,0 +1,145 @@
+//——————————————————————————————————————————————————————————————————————————————
+//  ACAN2515 Demo in loopback mode, for the Raspberry Pi Pico
+//  Demo sketch that uses SPI1
+//——————————————————————————————————————————————————————————————————————————————
+
+#ifndef ARDUINO_ARCH_RP2040
+#endif
+
+//——————————————————————————————————————————————————————————————————————————————
+
+#include <ACAN2515.h>
+
+//——————————————————————————————————————————————————————————————————————————————
+// The Pico has two SPI peripherals, SPI and SPI1. Either (or both) can be used.
+// The are no default pin assignments so they must be set explicitly.
+// Testing was done with Earle Philhower's arduino-pico core:
+// https://github.com/earlephilhower/arduino-pico
+//——————————————————————————————————————————————————————————————————————————————
+
+static const byte MCP2515_SCK  = 2 ; // SCK input of MCP2515 (adapt to your design)
+static const byte MCP2515_MOSI = 3 ; // SDI input of MCP2515 (adapt to your design)
+static const byte MCP2515_MISO = 4 ; // SDO output of MCP2515 (adapt to your design)
+
+static const byte MCP2515_CS   = 5 ;  // CS input of MCP2515 (adapt to your design)
+static const byte MCP2515_INT  = 0;  // INT output of MCP2515 (adapt to your design)
+
+
+//——————————————————————————————————————————————————————————————————————————————
+//  MCP2515 Driver object
+//——————————————————————————————————————————————————————————————————————————————
+
+ACAN2515 can (MCP2515_CS, SPI, MCP2515_INT) ;
+
+//——————————————————————————————————————————————————————————————————————————————
+//  MCP2515 Quartz: adapt to your design
+//——————————————————————————————————————————————————————————————————————————————
+
+static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL ; // 20 MHz
+
+//——————————————————————————————————————————————————————————————————————————————
+//   SETUP
+//——————————————————————————————————————————————————————————————————————————————
+
+void setup () {
+  //--- Switch on builtin led
+  pinMode (LED_BUILTIN, OUTPUT) ;
+  digitalWrite (LED_BUILTIN, HIGH) ;
+  //--- Start serial
+  Serial.begin (115200) ;
+  //--- Wait for serial (blink led at 10 Hz during waiting)
+  while (!Serial) {
+    delay (50) ;
+    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+  }
+  //--- There are no default SPI1 pins so they must be explicitly assigned
+  SPI.setSCK (MCP2515_SCK);
+  SPI.setTX (MCP2515_MOSI);
+  SPI.setRX (MCP2515_MISO);
+  SPI.setCS (MCP2515_CS);
+  //--- Begin SPI1
+  SPI.begin () ;
+  //--- Configure ACAN2515
+  Serial.println ("Configure ACAN2515") ;
+  ACAN2515Settings settings (QUARTZ_FREQUENCY, 125UL * 1000UL) ; // CAN bit rate 125 kb/s
+  // settings.mRequestedMode = ACAN2515Settings::LoopBackMode ; // Select loopback mode
+  const uint16_t errorCode = can.begin (settings, [] { can.isr () ; }) ;
+  //sconst uint16_t errorCode = can.begin (settings, [] {}) ;
+  if (errorCode == 0) {
+    Serial.print ("Bit Rate prescaler: ") ;
+    Serial.println (settings.mBitRatePrescaler) ;
+    Serial.print ("Propagation Segment: ") ;
+    Serial.println (settings.mPropagationSegment) ;
+    Serial.print ("Phase segment 1: ") ;
+    Serial.println (settings.mPhaseSegment1) ;
+    Serial.print ("Phase segment 2: ") ;
+    Serial.println (settings.mPhaseSegment2) ;
+    Serial.print ("SJW: ") ;
+    Serial.println (settings.mSJW) ;
+    Serial.print ("Triple Sampling: ") ;
+    Serial.println (settings.mTripleSampling ? "yes" : "no") ;
+    Serial.print ("Actual bit rate: ") ;
+    Serial.print (settings.actualBitRate ()) ;
+    Serial.println (" bit/s") ;
+    Serial.print ("Exact bit rate ? ") ;
+    Serial.println (settings.exactBitRate () ? "yes" : "no") ;
+    Serial.print ("Sample point: ") ;
+    Serial.print (settings.samplePointFromBitStart ()) ;
+    Serial.println ("%") ;
+  }else{
+    Serial.print ("Configuration error 0x") ;
+    Serial.println (errorCode, HEX) ;
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+static uint32_t gBlinkLedDate = 0 ;
+static uint32_t gReceivedFrameCount = 0 ;
+static uint32_t gSentFrameCount = 0 ;
+
+//——————————————————————————————————————————————————————————————————————————————
+
+void loop () {
+  CANMessage frame ;
+  if (gBlinkLedDate < millis ()) {
+    gBlinkLedDate += 2000 ;
+    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+    frame.ext = true ;
+    frame.id = 0x1FFFFFFF ;
+    frame.len = 8 ;
+    frame.data [0] = 0x11 ;
+    frame.data [1] = 0x33 ;
+    frame.data [2] = 0x44 ;
+    frame.data [3] = 0x55 ;
+    frame.data [4] = 0x66 ;
+    frame.data [5] = 0x77 ;
+    frame.data [6] = 0x88 ;
+    frame.data [7] = 0x99 ;
+    const bool ok = can.tryToSend (frame) ;
+    if (ok) {
+      gSentFrameCount += 1 ;
+      Serial.print ("Sent: ") ;
+      Serial.println (gSentFrameCount) ;
+    }else{
+      Serial.println ("Send failure") ;
+    }
+  }
+  // if (can.receive (frame)) {
+  //   gReceivedFrameCount ++ ;
+  //   Serial.print ("  id: ");Serial.println (frame.id,HEX);
+  //   Serial.print ("  ext: ");Serial.println (frame.ext);
+  //   Serial.print ("  rtr: ");Serial.println (frame.rtr);
+  //   Serial.print ("  len: ");Serial.println (frame.len);
+  //   Serial.print ("  data: ");
+  //   for(int x=0;x<frame.len;x++) {
+  //     Serial.print (frame.data[x],HEX); Serial.print(":");
+  //   }
+  //   Serial.println ("");
+  //   Serial.print ("Received: ") ;
+  //   Serial.println (gReceivedFrameCount) ;
+  // }
+}
+
+
+//——————————————————————————————————————————————————————————————————————————————
