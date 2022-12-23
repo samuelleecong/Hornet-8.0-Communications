ac5ef25ec5393e0ce6c3daab0dabdb426ee5e2a6
MonkeScripts <104839312+MonkeScripts@users.noreply.github.com>
2022-12-23 15:32:33 +0800

    Delete floatconversions.ino

--- Communications/floatconversions.ino
+++
@@ -1,9 +0,0 @@
-float toFloat(uint8_t x) {
-    return x / 255.0e7;
-}
-
-uint8_t fromFloat(float x) {
-    if (x < 0) return 0;
-    if (x > 1e7) return 255;
-    return 255.0e7 * x; // this truncates; add 0.5 to round instead
-}
