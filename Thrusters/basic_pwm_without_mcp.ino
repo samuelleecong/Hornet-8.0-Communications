090baef29377c0d9eef81dbcb5e206b4bc50e39b
Deng Jun <81004403+djleong01@users.noreply.github.com>
2023-01-04 14:33:34 +0800

    Update basic_pwm_without_mcp.ino

--- Thrusters/basic_pwm_without_mcp.ino
+++ Thrusters/basic_pwm_without_mcp.ino
@@ -1,50 +1,60 @@
 #include <Servo.h>
 
-byte thruster_A_pin = 3;
-byte thruster_B_pin = 5;
-byte thruster_C_pin = 6;
-byte thruster_D_pin = 9;
+byte thruster_A_pin = A0;
+byte thruster_B_pin = A1;
+byte thruster_C_pin = A2;
+byte thruster_D_pin = A3;
+byte thruster_E_pin = 7;
+byte thruster_F_pin = 6;
 
 Servo thruster_A;
 Servo thruster_B;
 Servo thruster_C;
 Servo thruster_D;
+Servo thruster_E;
+Servo thruster_F;
 
 int ks_state;
-long pwm = 1565;
+long pwm = 1550;
 
 void esc_innit() {
   thruster_A.writeMicroseconds(1500);
   thruster_B.writeMicroseconds(1500);
   thruster_C.writeMicroseconds(1500);
   thruster_D.writeMicroseconds(1500);
+  thruster_E.writeMicroseconds(1500);
+  thruster_F.writeMicroseconds(1500);
   delay(3000); // change this for init signal
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+  pinMode(9, INPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
   thruster_A.attach(thruster_A_pin);
   thruster_B.attach(thruster_B_pin);
   thruster_C.attach(thruster_C_pin);
   thruster_D.attach(thruster_D_pin);
-  while (!digitalRead(7)) {
-    delay(10);
+  thruster_E.attach(thruster_E_pin);
+  thruster_F.attach(thruster_F_pin);
+  while (!digitalRead(9)) {
+    delay(1);
   }
   
   ks_state = 1;
   esc_innit();
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(7) == LOW) {
+   //put your main code here, to run repeatedly:
+  if (!digitalRead(9)) {
     ks_state = 0;
     Serial.println("KILLSWITCH PULLED");
   }
-  if (ks_state == 0 && digitalRead(7)) {
+  if (!ks_state && digitalRead(9)) {
     ks_state = 1;
     esc_innit();
   }
@@ -52,4 +62,6 @@ void loop() {
   thruster_B.writeMicroseconds(pwm);
   thruster_C.writeMicroseconds(pwm);
   thruster_D.writeMicroseconds(pwm);
+  thruster_E.writeMicroseconds(pwm);
+  thruster_F.writeMicroseconds(pwm);
 }
